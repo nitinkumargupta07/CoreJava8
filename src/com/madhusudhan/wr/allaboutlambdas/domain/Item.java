4f9af5744dbb5b029a2cadd78da0812ff5c354ba
nitin <nitinguptamca@gmail.com>
2018-12-12 08:52:29 +0530

    a

---
+++ src/com/madhusudhan/wr/allaboutlambdas/domain/Item.java
@@ -0,0 +1,92 @@
+package com.madhusudhan.wr.allaboutlambdas.domain;
+
+import java.math.BigDecimal;
+
+public class Item {
+	public Item(int id, String name, BigDecimal price, int quantity) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + quantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		return true;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	int id;
+	String name;
+	BigDecimal price;
+	int quantity;
+}
---
+++ src/com/madhusudhan/wr/allaboutlambdas/util/ItemUtil.java
@@ -0,0 +1,37 @@
+package com.madhusudhan.wr.allaboutlambdas.util;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.madhusudhan.wr.allaboutlambdas.domain.Item;
+
+public class ItemUtil {
+	public static List<Item> createItem() {
+		List<Item> items = new ArrayList<>();
+		Item item1 = new Item(1, "banana", new BigDecimal(12.75), 12);
+		Item item2 = new Item(2, "banana1", new BigDecimal(13.75), 2);
+		Item item3 = new Item(3, "banana2", new BigDecimal(14.75), 1);
+		Item item4 = new Item(4, "banana3", new BigDecimal(15.75), 122);
+		Item item5 = new Item(5, "banana4", new BigDecimal(16.75), 123);
+		Item item6 = new Item(6, "banana5", new BigDecimal(17.75), 412);
+		Item item7 = new Item(7, "banana6", new BigDecimal(18.75), 612);
+		Item item8 = new Item(8, "banana7", new BigDecimal(19.75), 712);
+		Item item9 = new Item(9, "banana8", new BigDecimal(129.75), 912);
+		Item item10 = new Item(10, "banana9", new BigDecimal(112.75), 912);
+		items.add(item1);
+		items.add(item2);
+		items.add(item3);
+		items.add(item4);
+		items.add(item5);
+		items.add(item6);
+		items.add(item7);
+		items.add(item8);
+		items.add(item9);
+		items.add(item10);
+		items.add(item5);
+		items.add(item6);
+		items.add(item7);
+		return items;
+	}
+}
